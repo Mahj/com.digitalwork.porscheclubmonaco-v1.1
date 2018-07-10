4a245834de3309087e94243b7e50fcf63c633a3a
Mahjoub EL ATTAR <mahj.attar@yahoo.fr>
2018-07-10 18:58:23 +0200

    Your Awesome Changes

---
+++ src/config/config.ts
@@ -0,0 +1,25 @@
+
+export const config = {
+    apiKey: "AIzaSyDJytTYaO7KLxxUtlN2J7t4MClTzc__QbQ",
+    authDomain: "porscheclubmonacomobilea-dedf5.firebaseapp.com",
+    databaseURL: "https://porscheclubmonacomobilea-dedf5.firebaseio.com",
+    projectId: "porscheclubmonacomobilea-dedf5",
+    storageBucket: "porscheclubmonacomobilea-dedf5.appspot.com",
+    messagingSenderId: "6242581575"
+};
+
+export const payPalEnvironmentSandbox = '<insert your key here>';
+
+export const webClientId = '<insert your key here>';
+
+export const admobAndroid = {
+    banner:       'ca-app-pub-<insert your key here>',
+    interstitial: 'ca-app-pub-<insert your key here>',
+    videos:       'ca-app-pub-<insert your key here>'
+};
+
+export const admobIOS = {
+    banner: 'ca-app-pub-<insert your key here>',
+    interstitial: 'ca-app-pub-<insert your key here>',
+    videos: ''
+};
